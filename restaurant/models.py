5e6490f0e6dd842814b2a95aa26eac6219112cda
fxrnando <vook2000@hotmail.com>
2023-08-25 12:26:53 -0500

    Agregado pruebas por unit test y comprobado con insomnia

--- restaurant/models.py
+++ restaurant/models.py
@@ -11,3 +11,6 @@ class MenuItem(models.Model):
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.SmallIntegerField()
+
+    def __str__(self):
+        return f'{self.title} : {str(self.price)}'
